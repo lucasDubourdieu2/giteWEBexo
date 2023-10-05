eac50387644630631a07b6c0b1ed51dfd9864710
lucasDubourdieu2 <dubourdl@3il.fr>
2023-10-05 11:55:45 +0200

    API carte et modif style

---
+++ js/carte.js
@@ -0,0 +1,22 @@
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 44.44914906971942;
+var lon = 2.4932902106158;
+var macarte = null;
+// Fonction d'initialisation de la carte
+function initMap() {
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    macarte = L.map('map').setView([lat, lon], 13);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(macarte);
+    // Nous ajoutons un marqueur
+    var marker = L.marker([lat, lon]).addTo(macarte).bindPopup("Gite Figuies").openPopup();
+}
+window.onload = function () {
+    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+    initMap();
+};
