e727415ae58ddced9cc31ef80dab3e667a8e1d00
lucasDubourdieu2 <dubourdl@3il.fr>
2023-10-27 10:03:12 +0200

    Mise a jour de l'index

--- view/newsletter.php
+++ view/newsletter.php
@@ -14,6 +14,7 @@
         <div class="container">
             <h1 class="title">Inscription à la Newsletter</h1>
             <p>Recevez nos dernières mises à jour par e-mail en vous inscrivant à notre newsletter.</p>
+            <br>
             <form action="../controller/verifNewsletter.php" method="post">
                 <label for="nom">Nom :</label>
                 <input type="text" id="nom" name="nom" required>
@@ -22,7 +23,7 @@
                 <input type="email" id="email" name="email" required>
                 <br>
                 <input type="checkbox" id="consentement" name="consentement" required>
-                <label for="consentement">Je consens à recevoir des e-mails de la newsletter.</label>
+                <label for="consentement" >Je consens à recevoir des e-mails de la newsletter.</label>
                 <br>
                 <input type="submit" class="btnNewsletter" value="S'inscrire">
             </form>
